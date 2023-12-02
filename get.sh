cb39c837fb78f421b3c68e05d0a40ca070605645
yimi <yimi8864@gmail.com>
2023-12-02 15:05:42 +0800

    test action

---
+++ get.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ARCH=$(uname -m)
+
+if [ "$ARCH" == "x86_64" ]; then
+  File="goForward.zip"
+elif [ "$ARCH" == "aarch64" ]; then
+  File="goForward_arm64.zip"
+else
+  echo "Unsupported architecture: $ARCH"
+  exit 1
+fi
+
+# Download and unzip
+wget "https://github.com/csznet/goForward/releases/latest/download/$File" && unzip "$File" && rm "$File"
+
+# Set permissions
+chmod +x goForward
+
+echo "successfully."
