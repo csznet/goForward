0ff1fd455bcbde6108b267e2fc2ca11070b88c2c
yimi <yimi8864@gmail.com>
2023-12-03 01:35:16 +0800

    add unzip sup

--- get.sh
+++ get.sh
@@ -11,10 +11,25 @@ else
   exit 1
 fi
 
+# Check if unzip is installed
+if ! command -v unzip &> /dev/null; then
+  echo "unzip is not installed. Installing..."
+  
+  # Install unzip based on the package manager
+  if command -v apt-get &> /dev/null; then
+    sudo apt-get install -y unzip
+  elif command -v yum &> /dev/null; then
+    sudo yum install -y unzip
+  else
+    echo "Unsupported package manager. Please install unzip manually."
+    exit 1
+  fi
+fi
+
 # Download and unzip
 wget "https://github.com/csznet/goForward/releases/latest/download/$File" && unzip "$File" && rm "$File"
 
 # Set permissions
 chmod +x goForward
 
-echo "successfully."
+echo "Suc!"
