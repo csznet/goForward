eee62a4c63a211db113b0fc1ae472e4ee5a5de35
yimi <yimi8864@gmail.com>
2023-12-03 17:04:01 +0800

    美化

--- get.sh
+++ get.sh
@@ -3,33 +3,44 @@
 ARCH=$(uname -m)
 
 if [ "$ARCH" == "x86_64" ]; then
-  File="goForward.zip"
+  FILE="goForward.zip"
 elif [ "$ARCH" == "aarch64" ]; then
-  File="goForward_arm64.zip"
+  FILE="goForward_arm64.zip"
 else
-  echo "Unsupported architecture: $ARCH"
+  echo -e "\e[41mError\e[0m: Unsupported architecture: $ARCH"
   exit 1
 fi
 
 # Check if unzip is installed
 if ! command -v unzip &> /dev/null; then
-  echo "unzip is not installed. Installing..."
-  
+  echo -e "\e[41mError\e[0m: unzip is not installed. Installing..."
+
   # Install unzip based on the package manager
   if command -v apt-get &> /dev/null; then
     sudo apt-get install -y unzip
   elif command -v yum &> /dev/null; then
     sudo yum install -y unzip
   else
-    echo "Unsupported package manager. Please install unzip manually."
+    echo -e "\e[41mError\e[0m: Unsupported package manager. Please install unzip manually."
     exit 1
   fi
 fi
 
 # Download and unzip
-wget "https://github.com/csznet/goForward/releases/latest/download/$File" && unzip "$File" && rm "$File"
+if ! wget "https://github.com/csznet/goForward/releases/latest/download/$FILE"; then
+  echo -e "\e[41mError\e[0m: Failed to download $FILE. Please check your internet connection or try again later."
+  exit 1
+fi
+
+if ! unzip "$FILE"; then
+  echo -e "\e[41mError\e[0m: Failed to unzip $FILE."
+  exit 1
+fi
+
+rm "$FILE"
 
 # Set permissions
 chmod +x goForward
 
-echo "Suc!"
+# Output success message
+echo -e "\e[44mSuccess\e[0m: The 'goForward' executable is ready for use."
